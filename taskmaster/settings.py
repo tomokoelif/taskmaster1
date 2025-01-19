57cf4ec80f3a048af41b26b5506e636237a344cb
Tomoko Koide <tomokoelif@gmail.com>
2025-01-19 17:03:01 +0000

    Enable debug mode in settings and remove commented database configuration

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u3fqj1o5i75d2b4t#y39o@vg(iq#w@4&w)h7_z@5_n%i8kssi*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -77,13 +77,6 @@ WSGI_APPLICATION = 'taskmaster.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
